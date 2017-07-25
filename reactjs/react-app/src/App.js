f9f467de8b9436e0153389041b8a5dc956002bff
hussain <ask2md@gmail.com>
2017-07-26 00:27:05 +0530

    using props.children

--- reactjs/react-app/src/App.js
+++ reactjs/react-app/src/App.js
@@ -1,22 +1,4 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 class App extends Component {
   constructor(){
@@ -35,15 +17,27 @@ class App extends Component {
     //let defd = this.props.defd
     return (
         <div>
-          <input type="text" onClick={this.update.bind(this)} />
           <h1>Hello React World!!</h1>
           <h2>{txt} and {this.props.cat} and {this.props.defd}</h2>
           <h3>{this.state.stateText}</h3>
           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+          <Widget update = {this.update.bind(this)} /><br/>
+          <Widget update = {this.update.bind(this)} />
+          <Button>I <Heart /> React</Button>
         </div>
     )
   }
 }
+class Heart extends Component {
+  render(){
+    return <span>&hearts;</span>
+  }
+}
+
+const Button = (props) => <button>{props.children}</button>
+
+const Widget = (props) =>
+    <input type="text" onChange={props.update} />
 
 App.propTypes = {
   txt : React.PropTypes.string,
@@ -52,6 +46,6 @@ App.propTypes = {
 App.defaultProps = {
   defd : "This is the default text"
 }
-// const App = () => <h1>Hello React</h1>;
+// const App = () => <h1>Hello React</h1>;    // stateless function component
 
 export default App;
