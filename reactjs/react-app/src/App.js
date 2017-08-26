f5a2fd8e49824e3b33b4ed522e4cf12bc96906bb
hussain <ask2md@gmail.com>
2017-08-26 11:51:20 +0530

    use map to create react component from arrays of data

--- reactjs/react-app/src/App.js
+++ reactjs/react-app/src/App.js
@@ -146,5 +146,39 @@ class App1 extends Component {
 
 App1.defaultProps = { val : 0 }
 
+//export default App1
 
-export default App1
+class App2 extends Component {
+    constructor(){
+      super();
+      this.state = {
+          items : []
+      }
+    }
+    componentWillMount(){
+      fetch('http://swapi.co/api/people/?format.json')
+        .then( response => response.json())
+        .then( ({results: items}) => this.setState({items}) )
+    }
+    filter(e){
+        this.setState({filter: e.target.value})
+    }
+    render(){
+      let items = this.state.items;
+      if(this.state.filter){
+        items= items.filter( item =>
+            item.name.toLowerCase()
+              .includes(this.state.filter.toLowerCase()))
+      }
+      return (
+        <div>
+          <input type="text" onChange={this.filter.bind(this)}/><br />
+          {/*{items.map(item => <h4 key={item.name}>{item.name}</h4>)} */}
+          {items.map(item => <Person key={item.name} person={item}/>)}
+        </div>
+      )
+    }
+}
+
+const Person = (props) => <h4>{props.person.name}</h4>
+export default App2;
