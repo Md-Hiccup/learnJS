819b0b74c41b3747cd89e378f071ff9d1ec11db2
Hussain <ask2md@gmail.com>
2017-09-27 12:34:20 +0530

    rename react-redux

---
+++ reactjs/react-Reduxx/src/component/layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Layout extends Component {
+    render(){
+        return (
+            <div className="container">
+                <h2>Main page Layout</h2>
+            </div>
+        )
+    }
+}
+
+export default Layout;
