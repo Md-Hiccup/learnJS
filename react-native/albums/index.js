6400295eb73a7591418cf82191cb9ab94c68903d
Md-Hiccup <ask2md@gmail.com>
2018-03-16 19:39:59 +0530

    create & styled header

--- react-native/albums/index.js
+++ react-native/albums/index.js
@@ -1,4 +1,15 @@
+// Import a library to help create a Component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
-
+import Header from './src/components/header';
+// Create a Component
+const App = () => (
+    <Header headerText={'Ablums'} />
+);
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('albums', () => App);
