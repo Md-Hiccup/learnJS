884c2ff383e95a62ffbd8b75b132ef93e94ff113
Md-Hiccup <ask2md@gmail.com>
2018-03-19 19:51:52 +0530

    lip: done local auth

--- react-native/auth/src/App.js
+++ react-native/auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyA66aZP9YvjNWoB3gxTEPdbU9nooscmzXA",
@@ -14,12 +16,38 @@ class App extends Component {
       storageBucket: "authentication-2a3fb.appspot.com",
       messagingSenderId: "426444523169"
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({ loggedIn: true});
+      } else {
+        this.setState({ loggedIn: false});
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render(){
     return (
       <View>
-        <Header headerText = 'Authentication' />
-        <LoginForm />
+        <Header headerText= 'Authentication' />
+        {this.renderContent()}
       </View>
     )
   }
