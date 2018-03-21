85b709eb82ac905628d1fc1c50111706673fc049
Md-Hiccup <ask2md@gmail.com>
2018-03-22 00:37:48 +0530

    lip: learned navigation & firebase storage

--- react-native/manager/src/App.js
+++ react-native/manager/src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import firebase from 'firebase'
+import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Routes from './Router.js';
 
 class App extends Component {
   componentWillMount() {
     // Initialize Firebase
     const config = {
-      apiKey: "AIzaSyDuVpt1QeeVLZIYunIubbls2X2vFPns1f4",
-      authDomain: "manager-ddc65.firebaseapp.com",
-      databaseURL: "https://manager-ddc65.firebaseio.com",
-      projectId: "manager-ddc65",
-      storageBucket: "",
-      messagingSenderId: "413831030864"
+      apiKey: 'AIzaSyDuVpt1QeeVLZIYunIubbls2X2vFPns1f4',
+      authDomain: 'manager-ddc65.firebaseapp.com',
+      databaseURL: 'https://manager-ddc65.firebaseio.com',
+      projectId: 'manager-ddc65',
+      storageBucket: '',
+      messagingSenderId: '413831030864'
     };
     firebase.initializeApp(config);
   }
   render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk)); // 3 arg. is for store enhancer
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Routes />
       </Provider>
-    )
+    );
   }
 }
 
--- react-native/manager/src/reducers/AuthReducer.js
+++ react-native/manager/src/reducers/AuthReducer.js
@@ -10,9 +10,9 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
+  // console.log(action);
 
-  switch(action.type){
+  switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
@@ -22,7 +22,10 @@ export default (state = INITIAL_STATE, action) => {
     case LOGIN_USER_SUCCESS:
       return { ...state, ...INITIAL_STATE, user: action.payload };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed.', password: '', loading: false }
+      return { ...state,
+        error: 'Authentication Failed.',
+        password: '',
+        loading: false };
     default:
       return state;
   }
